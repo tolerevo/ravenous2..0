b9144233919969a66258604d21ecf46b0d85904f
tolerevo <tolerevo@hotmail.com>
2024-01-09 13:02:33 -0600

    end static components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,13 @@
 import React /*{ useState }*/ from 'react';
 import styles from './App.module.css';
 import BusinessList from '../BusinessList/BusinessList';
-
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>Ravenous</h1>
+      <SearchBar />
       <BusinessList />
     </div>
   );
