36270b709055e1d457bbbacc30fdb4f6fc937c3d
tolerevo <tolerevo@hotmail.com>
2024-01-09 16:36:46 -0600

    pass props from top to bottom

--- src/Components/Business/Business.js
+++ src/Components/Business/Business.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import styles from './Business.module.css';
 
-const business = {
-	imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-	name: 'MarginOtto Pizzeria',
-	address: '1010 Paddington Way',
-	city: 'Flavortown',
-	state: 'NY',
-	zipCode: '10101',
-	category: 'Italian',
-	rating: 4.5,
-	reviewCount: 90
-}
-
-const Business = () => {
+const Business = ({ business }) => {
 	return (
 		<div className={styles.Business}>
 			<div className={styles.imageContainer}>
--- src/Components/BusinessList/BusinessList.js
+++ src/Components/BusinessList/BusinessList.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Business from '../Business/Business';
 import styles from './BusinessList.module.css';
 
-function BusinessList() {
+function BusinessList({ businesses }) {
   return (
     <div className={styles.BusinessList}>
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
+      {businesses.map((business) => {
+        return <Business business={business} key={business.name} />
+      })}
     </div>
   );
 }
